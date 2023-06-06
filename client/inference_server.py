3e9994ff30e07a950019d852048a5f2a50bdd532
Martin Berki <93865363+mrtynx@users.noreply.github.com>
2023-06-06 09:04:04 +0200

    Update inference_server.py

--- client/inference_server.py
+++ client/inference_server.py
@@ -1,20 +1,47 @@
+import sys
 import cv2
-import torch
 import socket
 import pickle
 import struct
+import argparse
+import numpy as np
+import tensorflow as tf
 
 from ultralytics import YOLO
 
 BUF_SIZE = 4096
+
+# Create the parser
+parser = argparse.ArgumentParser(description="Program description")
+
+# Create a mutually exclusive group
+group = parser.add_mutually_exclusive_group()
+
+# Add the options to the group
+group.add_argument("--detect", action="store_true", help="Option A description")
+group.add_argument("--classify", action="store_true", help="Option B description")
+
+# Parse the command-line arguments
+args = parser.parse_args()
+
+# Check which option was chosen
+if args.detect:
+    print("Using YOLO detection")
+    model = YOLO("yolov8n.pt")
+
+elif args.classify:
+    print("Using CNN classification")
+    model = tf.keras.models.load_model("../ml/classifier/mdl/classifier_mdl.h5")
+
+else:
+    print("No mode chosen ['--detect', '--classify']")
+    sys.exit()
 
 
 def main():
     # Configuration
     host = "127.0.0.1"
     port = 6666
-
-    model = YOLO("yolov8n.pt")
 
     # Create a socket
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -39,16 +66,24 @@ def main():
             print("Error: Incomplete data received")
         else:
             received = pickle.loads(data)
-            results = model.predict(received, imgsz=640, conf=0.5)
-            res_plotted = results[0].plot()
-            cv2.imshow("result", res_plotted)
-            # client_socket.sendall(b"niceru\n")
+            if args.detect:
+                results = model.predict(received, imgsz=640, conf=0.5)
+                res_plotted = results[0].plot()
+                cv2.imshow("result", res_plotted)
+                # client_socket.sendall(b"niceru\n")
 
-            if cv2.waitKey(1) & 0xFF == ord("q"):
-                cv2.destroyAllWindows()
-                server_socket.close()
-                break
+                if cv2.waitKey(1) & 0xFF == ord("q"):
+                    cv2.destroyAllWindows()
+                    server_socket.close()
+                    break
+            elif args.classify:
+                result = model.predict(np.expand_dims(received, axis=0), verbose=0)
+                predicted_class = np.argmax(result, axis=1)
+                print(predicted_class)
+                client_socket.sendall(predicted_class[0])
+                # confidence = np.max(predictions)
 
 
 if __name__ == "__main__":
     main()
+
