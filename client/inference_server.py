f4ebba6e277cb197ee555c14e25c1b1acfde5fbe
xberki <81830073+xberki@users.noreply.github.com>
2023-06-19 11:12:17 +0200

    Revert inference server

--- client/inference_server.py
+++ client/inference_server.py
@@ -1,30 +1,13 @@
 import sys
 import cv2
-import time
 import socket
 import pickle
 import struct
 import argparse
 import numpy as np
 import tensorflow as tf
-import torch
-from PIL import Image
-
-from torchvision.transforms import Compose, Resize, ToTensor
 
 from ultralytics import YOLO
-
-
-def recvall(sock, count):
-    buf = b""
-    while count:
-        newbuf = sock.recv(count)
-        if not newbuf:
-            return None
-        buf += newbuf
-        count -= len(newbuf)
-    return buf
-
 
 BUF_SIZE = 4096
 
@@ -41,14 +24,10 @@ group.add_argument("--classify", action="store_true", help="Option B description
 # Parse the command-line arguments
 args = parser.parse_args()
 
-
 # Check which option was chosen
 if args.detect:
     print("Using YOLO detection")
     model = YOLO("best.pt")
-    # model = torch.hub.load('client/yolov5', 'custom', path='client/yolov5/best.pt', source='local')
-    # model.to(device)
-    # model.eval()
 
 elif args.classify:
     print("Using CNN classification")
@@ -69,27 +48,11 @@ def main():
     server_socket.bind((host, port))
     server_socket.listen()
     print(f"[*] Listening on {host}:{port}")
-    client_socket, client_address = None, None
 
-    while client_socket is None:
-        try:
-            client_socket, client_address = server_socket.accept()
-        except Exception as e:
-            print(f"Failed to establish connection: {e}")
-            continue
-
-    print(f"Client has connected: {client_address}")
-
+    client_socket, client_address = server_socket.accept()
     while True:
-        # client_socket, client_address = server_socket.accept()
-        # data_size_bytes = client_socket.recv(4)
-        # data_size = struct.unpack("!I", data_size_bytes)[0]
-        data_size_bytes = recvall(client_socket, 4)
-        if data_size_bytes is not None:
-            data_size = struct.unpack("!I", data_size_bytes)[0]
-        else:
-            print("No data received. Connection may have been closed.")
-            break
+        data_size_bytes = client_socket.recv(4)
+        data_size = struct.unpack("!I", data_size_bytes)[0]
 
         data = b""
         while len(data) < data_size:
@@ -102,33 +65,36 @@ def main():
         if len(data) != data_size:
             print("Error: Incomplete data received")
         else:
-            try:
-                received = pickle.loads(data)
-                if args.detect:
-                    results = model.predict(received, imgsz=640, conf=0.5)
-                    res_plotted = results[0].plot()
-                    res_plotted = cv2.cvtColor(res_plotted, cv2.COLOR_BGR2RGB)
+            received = pickle.loads(data)
+            if args.detect:
+                results = model.predict(received, imgsz=640, conf=0.5)
+                dat = results[0].boxes.data
+                labels = dat[:, -1].numpy().astype(np.uint8)
+                print(f"Labels {labels}\n")
+                if len(labels) > 0:
+                    hist, bins = np.histogram(
+                        labels, bins=max(labels) + 1, range=(0, max(labels) + 1)
+                    )
+                    print("Bins:", bins)
+                    print("Counts:", hist)
+                else:
+                    hist = np.zeros(8)
 
-                    print("NAMES\n")
-                    dat = results[0].boxes.data
-                    labels = dat[:, -1].numpy().astype(np.uint8)
-                    print(f"Labels {labels}\n")
-                    if len(labels) > 0:
-                        hist, bins = np.histogram(
-                            labels, bins=max(labels) + 1, range=(0, max(labels) + 1)
-                        )
-                        print("Bins:", bins)
-                        print("Counts:", hist)
-                        client_socket.sendall(hist.astype(np.uint8))
+                client_socket.sendall(hist.astype(np.uint8))
+                res_plotted = results[0].plot()
+                cv2.imshow("result", res_plotted)
 
-                elif args.classify:
-                    result = model.predict(np.expand_dims(received, axis=0), verbose=0)
-                    predicted_class = np.argmax(result, axis=1)
-                    print(predicted_class)
-                    client_socket.sendall(predicted_class[0])
-                    # confidence = np.max(predictions)
-            except Exception as error:
-                print("Error occured: " + error.with_traceback)
+                if cv2.waitKey(1) & 0xFF == ord("q"):
+                    cv2.destroyAllWindows()
+                    server_socket.close()
+                    break
+
+            elif args.classify:
+                result = model.predict(np.expand_dims(received, axis=0), verbose=0)
+                predicted_class = np.argmax(result, axis=1)
+                print(predicted_class)
+                client_socket.sendall(predicted_class[0])
+                # confidence = np.max(predictions)
 
 
 if __name__ == "__main__":
