4b41885e5d2c30e1cb0d96d37ff1cc70f614e404
Martin Berki <93865363+mrtynx@users.noreply.github.com>
2023-06-20 21:17:16 +0200

    Update model_cnn.py

--- ml/classifier/model_cnn.py
+++ ml/classifier/model_cnn.py
@@ -1,52 +1,103 @@
 import tensorflow as tf
 
-from img_dataset import *
+from datetime import datetime
 import pathlib
 
+import seaborn as sns
+import numpy as np
+
 from tensorflow.keras.preprocessing import image as image_utils
-from keras.callbacks import ModelCheckpoint
+from keras.callbacks import ModelCheckpoint, TensorBoard
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from sklearn.metrics import confusion_matrix
 
-path = pathlib.Path("sample_img/")
-num_classes = 9
-img_width = 640
-img_height = 640
-batch_size = 16
+import matplotlib.pyplot as plt
+import matplotlib
 
-dataset = tf.keras.preprocessing.image_dataset_from_directory(
-    directory=path, image_size=(img_height, img_width), batch_size=batch_size
-).map(preprocess_images)
+logdir = "logs/training/" + datetime.now().strftime("%Y%m%d-%H%M%S")
 
-val_split = 0.2
-num_samples = dataset.cardinality().numpy()
-num_val_samples = int(num_samples * val_split)
-num_train_samples = num_samples - num_val_samples
+path = pathlib.Path("sample_img_2/")
+num_classes = 2
+img_width = 512
+img_height = 512
+BATCH_SIZE = 16
+IMG_SIZE = (img_width, img_height)
 
-train_dataset = dataset.take(num_train_samples)
-val_dataset = dataset.skip(num_train_samples)
+data_gen = ImageDataGenerator(
+    validation_split=0.2,  # 20% of data will be used for validation
+)
 
+train_data = data_gen.flow_from_directory(
+    "sample_img",
+    target_size=IMG_SIZE,
+    batch_size=BATCH_SIZE,
+    subset="training",
+    color_mode="grayscale",
+    shuffle=True,
+    class_mode="categorical",
+)
+
+val_data = data_gen.flow_from_directory(
+    "sample_img",
+    target_size=IMG_SIZE,
+    batch_size=BATCH_SIZE,
+    subset="validation",
+    color_mode="grayscale",
+    shuffle=True,
+    class_mode="categorical",
+)
 
 model = tf.keras.Sequential(
     [
-        tf.keras.layers.InputLayer(input_shape=(img_height, img_width, 3)),
+        tf.keras.layers.InputLayer(input_shape=(img_height, img_width, 1)),
         tf.keras.layers.Conv2D(32, 3, activation="relu"),
-        tf.keras.layers.MaxPooling2D(),
+        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
         tf.keras.layers.Conv2D(64, 3, activation="relu"),
-        tf.keras.layers.MaxPooling2D(),
+        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+        tf.keras.layers.Conv2D(128, 3, activation="relu"),
+        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+        tf.keras.layers.Conv2D(256, 3, activation="relu"),
+        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
         tf.keras.layers.Flatten(),
-        tf.keras.layers.Dense(128, activation="relu"),
-        tf.keras.layers.Dense(num_classes, activation="softmax"),
+        tf.keras.layers.Dense(512, activation="relu"),
+        tf.keras.layers.Dense(2, activation="softmax"),
     ]
 )
 
-checkpoints = [
 
-    ModelCheckpoint("../classifier_mdl.h5", save_best_only=True)
+callbacks = [
+    ModelCheckpoint("../classifier_mdl.h5", save_best_only=True, monitor="val_loss"),
+    TensorBoard(log_dir=logdir),
 ]
 
-model.compile(
-    optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"]
-)
+model.compile(optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"])
 
 history = model.fit(
-    train_dataset, epochs=30, validation_data=val_dataset, batch_size=batch_size, callbacks=checkpoints
+    train_data, epochs=30, validation_data=val_data, callbacks=callbacks
 )
+
+# model = tf.keras.models.load_model("classifier_mdl.h5")
+true_labels = val_data.classes
+probabilities = model.predict(val_data)
+predicted_labels = np.argmax(probabilities, axis=1)
+cm = confusion_matrix(true_labels, predicted_labels)
+
+plt.figure(figsize=(2, 2))
+sns.heatmap(cm, annot=True, fmt="d", cmap="Blues")
+plt.title("Validation confusion matrix")
+plt.ylabel("Actual label")
+plt.xlabel("Predicted label")
+plt.show()
+
+
+true_labels = train_data.classes
+probabilities = model.predict(train_data)
+predicted_labels = np.argmax(probabilities, axis=1)
+cm = confusion_matrix(true_labels, predicted_labels)
+
+plt.figure(figsize=(2, 2))
+sns.heatmap(cm, annot=True, fmt="d", cmap="Blues")
+plt.title("Train confusion matrix")
+plt.ylabel("Actual label")
+plt.xlabel("Predicted label")
+plt.show()
